deca65ef0659f9fa75849b5ab2fea05d5a990540
Yusuf Atak <yusufatak026@gmail.com>
2023-07-17 22:40:45 +0300

    kod hariç giriş erkanı oluşturuldu

---
+++ Assets/Scripts/PlayExitScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UnityEngine.SceneManagement;
+
+ public void PlayGame()
+{
+
+    //Game scene number
+    SceneManager.LoadScene(0);
+}
